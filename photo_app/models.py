8eb0331901350310e5cfe74a45959985963c6175
Sergiy Romanenko <sergiy.romanenko@dish.com>
2019-08-30 11:42:18 +0300

    add bd

--- photo_app/models.py
+++ photo_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Picture(models.Model):
+    description = models.CharField(max_length=300)
+    field = models.ImageField(upload_to='media/', null=True)
+    
